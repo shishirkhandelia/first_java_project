61cd6e510c4a1586f85c4bdfc8ec695da79290ad
shishirkhandelia <60283133+shishirkhandelia@users.noreply.github.com>
2024-04-11 19:38:50 +0530

    Add files via upload

---
+++ src/test/java/com/shishir/first_java_project/FirstJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shishir.first_java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
